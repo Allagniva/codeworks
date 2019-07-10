5b0a4117c90263575993f35eb90ba729f3e663c3
Allagniva <52733338+Allagniva@users.noreply.github.com>
2019-07-10 11:36:03 +0530

    Add files via upload

---
+++ ESD_Project__2.ino
@@ -0,0 +1,28 @@
+#define BLYNK_PRINT Serial
+#include<ESP8266WiFi.h>
+#include<BlynkSimpleEsp8266.h>
+char auth[]="359d90c136ef4d33bb93a7406161f12d";
+char ssid[]="Omega";
+char pass[]="Alpha123456";
+//int IR=D4;
+int L1=D1;
+int L2=D0;
+void setup() 
+{
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);   
+}
+
+void loop() 
+{
+  
+  Blynk.run();
+  //digitalWrite(L1, HIGH); 
+  //Blynk.virtualWrite(V0,a);
+  //if(a==0)
+  //{
+  // Blynk.notify("Ghost is here"); 
+  //}
+}
